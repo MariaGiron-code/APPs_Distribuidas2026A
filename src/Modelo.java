c0f5132c962811bd01f42d83a65793eb814d9c03
MariaGiron-code <1202mariag@gmail.com>
2026-04-06 15:59:53 -0500

    Actualización de archivos del proyecto Calculator

---
+++ src/Modelo.java
@@ -0,0 +1,18 @@
+public class Modelo {
+
+    public double sumar(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public double restar(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public double dividir(double n1, double n2) {
+        return n1 / n2;
+    }
+}
